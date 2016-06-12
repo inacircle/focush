7b2dc0244e0983b2581f6eb11e367e08119b2daf
Keegan Lowenstein <keegan@bugsnag.com>
2016-06-12 00:25:47 -0700

    init mvp

---
+++ focu.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+while true; do
+  echo '>'
+  # check for presence of ~/.focush
+  # if found, say: you should be focusing on: {read contents of file}
+  if [ -e ~/.focush ]
+  then
+    say "you should be focusing on"
+    cat ~/.focush | say
+  fi
+  sleep 120
+done
